9835ae353a40622a54fdb5572bb81a27ebb10047
IMDP <duncan@dinegigs.com>
2020-01-07 14:18:42 -0500

    s85ch6

--- starter/app.js
+++ starter/app.js
@@ -70,7 +70,7 @@ var UIController = (function() {
         return {
             type: document.querySelector(DOMstrings.inputType).value, //Will be inc or exp
             description: document.querySelector(DOMstrings.inputDescription).value,
-            value: document.querySelector(DOMstrings.inputValue).value
+            value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -135,26 +135,43 @@ var controller = (function(budgetCtrl, UICtrl) {
         }
     });
 };
+    var updateBudget = function() {
+        
+        // 1. Calculate the budget
+
+
+        // 2. Return the budget
+
+
+        // 3. Display the budget on the UI
+
+
+    }
+
 
     var ctrlAddItem = function() {
         var input, newItem; 
        
         // 1. Get field input data
         var input = UICtrl.getInput();
+
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0 ) {
+            // 2. Add item to budget controller
+            var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+            // 3. add item to the UI
+            UICtrl.addListItem(newItem, input.type);
+
+            //4. Clear fields after adding to the UI
+            UICtrl.clearFields();
+
+            // 5. Calculate and update budget
+            updateBudget();
+
+            // 6. Display Budget on UI
+        }
        
-        // 2. Add item to budget controller
-        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-
-        // 3. add item to the UI
-        UICtrl.addListItem(newItem, input.type);
-
-        //4. Clear fields after adding to the UI
-        UICtrl.clearFields();
-
-        // 5. Calculate budget
-
-
-        // 6. Display Budget on UI
+        
     };
 
     return {
